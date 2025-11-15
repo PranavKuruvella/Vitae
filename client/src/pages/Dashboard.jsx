f5ef01c5e401822e0d253ee1891db3c16195eff1
pranavkuruvella <pranavkuruvella@gmail.com>
2025-11-15 17:00:17 +0530

    fixed dashboard-pt1

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,165 @@
-import React from 'react'
+import {
+  FilePenLineIcon,
+  PencilIcon,
+  PlusIcon,
+  TrashIcon,
+  UploadCloudIcon,
+  XCircleIcon,
+  XIcon,
+} from "lucide-react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { dummyResumeData } from "../assets/assets.js";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [allResumes, setAllResumes] = useState([]);
 
-export default Dashboard
+  const [showCreateResume, setshowCreateResume] = useState(false);
+  const [showUploadResume, setShowUploadResume] = useState(false);
+  const [title, setTitle] = useState("");
+  const [resume, setResume] = useState(null);
+  const [editResumeId, setEditResumeId] = useState("");
+
+  const navigate = useNavigate();
+  const colors = [
+    "#4B0082", // Indigo dye (darker variant, not the CSS indigo)
+    "#5D3FD3", // Majorelle Blue
+    "#3F00FF", // Electric Ultramarine
+    "#6F00FF", // Vivid Violet
+    "#7851A9", // Royal Purple
+  ];
+
+  const loadAllResumes = () => {
+    setAllResumes(dummyResumeData);
+  };
+
+  const createResume = async (e) => {
+    e.preventDefault();
+    setshowCreateResume(false);
+    navigate(`/app/builder/res123`);
+  };
+
+  useEffect(() => {
+    loadAllResumes();
+  }, []);
+
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto px-4 py-8">
+        <p className="text-2xl font-medium mb-6 bg-linear-to-r from bg-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden">
+          Welcome, John Doe
+        </p>
+
+        {/* // Create or Upload Resume Section */}
+        <div className="flex gap-4">
+          <button
+            onClick={() => setshowCreateResume(true)}
+            className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg
+          gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer"
+          >
+            <PlusIcon className="size-11 transition-all duration-300 p-2.5 bg-linear-to-br from-indigo-300 to-indigo-500 text-white rounded-full" />
+            <p className="text-sm group-hover:text-indigo-600 transition-all duration-300">
+              Create Resume
+            </p>
+          </button>
+
+          <button
+            className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg
+          gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer"
+          >
+            <UploadCloudIcon className="size-11 transition-all duration-300 p-2.5 bg-linear-to-br from-indigo-300 to-indigo-500 text-white rounded-full" />
+            <p className="text-sm group-hover:text-indigo-600 transition-all duration-300">
+              Upload Existing
+            </p>
+          </button>
+        </div>
+
+        <hr className="border-slate-300 my-6 sm:w-[305px]" />
+
+        <div className="grid grid-cols-2 sm:flex flex-wrap gap-4">
+          {allResumes.map((resume, index) => {
+            const baseColor = colors[index % colors.length];
+            return (
+              <button
+                key={index}
+                className="relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer "
+                style={{
+                  background: `linear-gradient(135deg,${baseColor}10,${baseColor}40)`,
+                  borderColor: baseColor + "40",
+                }}
+              >
+                <FilePenLineIcon
+                  className="size-7 group-hover:scale-105 transition-all"
+                  style={{ color: baseColor }}
+                />
+                <p
+                  className="text-sm group-hover:scale-105 transition-all px-2 text-center"
+                  style={{
+                    color: baseColor,
+                  }}
+                >
+                  {resume.title}
+                </p>
+
+                <p
+                  className="absolute bottom-1 text-[11px] text-slate-400 group-hover:text-slate-500 transition-all duration-300 px-2 text-center"
+                  style={{
+                    color: baseColor + "90",
+                  }}
+                >
+                  Updated on {new Date(resume.updatedAt).toLocaleDateString()}
+                </p>
+
+                <div className="absolute top-1 right-1 group-hover:flex items-center hidden">
+                  <TrashIcon className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors" />
+                  <PencilIcon className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors" />
+                </div>
+              </button>
+            );
+          })}
+        </div>
+
+        <div>
+          {showCreateResume && (
+            <form
+              onSubmit={createResume}
+              onClick={() => setshowCreateResume(false)}
+              className="fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center"
+            >
+
+              <div
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+                className="relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6"
+              >
+                <h2 className="text-xl font-bold mb-4">Create a New Resume</h2>
+                <input
+                  type="text"
+                  placeholder="Enter resume title"
+                  className="w-full px-4 py-2 mb-4 focus:border-indigo-600 ring-indigo-600"
+                  required
+                />
+
+                <button className="w-full py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 transition-colors">
+                  Create Resume
+                </button>
+
+                <XCircleIcon
+                  className="absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors"
+                  onClick={() => {
+                    setshowCreateResume(false);
+                    setTitle("");
+                  }}
+                />
+              </div>
+            </form>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Outlet, Link } from 'react-router-dom'
 
+import Navbar from '../components/NavBar.jsx'
+
 const Layout = () => {
   return (
-    <>
-    <h1>Layout</h1>
-    <Outlet />
-    </>
+    <div>
+      <div className='min-h-screen bg-gray-50'>
+        <Navbar />
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
