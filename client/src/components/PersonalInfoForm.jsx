3d54f8680e340769be00b4cfdfe0b7e279091d4e
pranavkuruvella <pranavkuruvella@gmail.com>
2025-12-01 13:56:42 +0530

    build personal details section in Resume Builder

---
+++ client/src/components/PersonalInfoForm.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { BriefcaseBusiness, Github, Globe, Linkedin, Mail, MapPin, Phone, User } from 'lucide-react'
+
+const PersonalInfoForm = ({ data, onChange, removeBackground, setRemoveBackground }) => {
+  // data: current form values | onChange: updates parent state
+  // removeBackground: toggle state | setRemoveBackground: toggle updater
+
+
+  const handleChange = (field, value) => { //change only the specific field
+    onChange({ ...data, [field]: value });
+  };
+
+
+  const fields = [ //personal info lo teskuntuna fields
+    { key: "full_name", label: "Full Name", icon: User, type: "text", required: true },
+    { key: "email", label: "Email Address", icon: Mail, type: "email", required: true },
+    { key: "phone", label: "Phone Number", icon: Phone, type: "tel" },
+    { key: "location", label: "Location", icon: MapPin, type: "text" },
+    { key: "profession", label: "Profession", icon: BriefcaseBusiness, type: "text" },
+    { key: "linkedin", label: "LinkedIn", icon: Linkedin, type: "url" },
+    { key: "github", label: "GitHub", icon: Github, type: "url" },
+    { key: "website", label: "Website", icon: Globe, type: "url" },
+  ]
+  return (
+    <div>
+      <h3 className='text-lg font-semibold text-gray-900'>Personal Information</h3>
+      <p className='text-sm text-gray-600'>Get Started with your Personal Information</p>
+
+      <div className='flex items-center gap-2'>
+        {/* image upload */}
+
+        <label>
+          {data.image ? (
+            // Show uploaded image preview
+            <img src={typeof data.image === "string" ? data.image : URL.createObjectURL(data.image)} alt="user Image" className='w-16 h-16 rounded-full object-cover mt-5 ring ring-slate-300 hover:opacity-80' />
+          ) : (
+            // Show placeholder if no image
+            <div className='inline-flex items-center gap-2 mt-5 text-slate-600 hover:text-slate-700 cursor-pointer'>
+              <User className='size-10 p-2.5 border rounded-full' />
+              Upload User Image
+            </div>
+          )}
+          <input type="file" accept="image/jpeg, image/png" onChange={(e) => handleChange("image", e.target.files[0])} className='hidden' /> {/* label medha nokithe avthundhi uploading */}
+        </label>
+
+        {/* remove background manam upload chesthe the img become an object -- such that we can see this toggle ,, db nunchi osthe img is a string -- so we cant see toggle */}
+        {
+          typeof data.image === "object" && (
+            <div className='flex flex-col gap-1 pl-4 text-sm'>
+              <div className='flex flex-col gap-2'>
+                <span className='text-gray-700 font-medium'>Remove Background</span>
+                <label className='relative inline-flex items-center cursor-pointer text-gray-900 gap-3' >
+                  <input type="checkbox" className='sr-only peer' onChange={(e) => setRemoveBackground(prev => !prev)} checked={removeBackground} />
+                  <div className='w-9 h-5 bg-gray-200 rounded-full peer peer-checked:bg-indigo-600 transition-all duration-200'></div>
+                  <span className='dot absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-4'></span>
+                </label>
+              </div>
+            </div>
+          )
+        }
+      </div>
+
+      {/* Loop through fields config to render inputs dynamically */}
+      {fields.map((field) => {
+        const Icon = field.icon;
+        return (
+          <div key={field.key} className="space-y-1 mt-5">
+            <label htmlFor={field.key} className='flex items-center gap-2 text-sm font-medium text-gray-600'>
+              <Icon className='size-4' />
+              {field.label}
+              {field.required && <span className='text-red-500'>*</span>}
+            </label>
+            <input type={field.type} id={field.key} value={data[field.key] || ""} onChange={(e) => handleChange(field.key, e.target.value)}
+              className='mt-1 w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring focus:ring-indigo-600 focus:border-indigo-600 outline-none transition-all text-sm' placeholder={`Enter your ${field.label.toLowerCase()}`} required={field.required} />
+
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default PersonalInfoForm
--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,9 +1,120 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
+import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from "lucide-react";
+import PersonalInfoForm from "../components/PersonalInfoForm.jsx";
 
 const ResumeBuilder = () => {
-  return (
-    <div>ResumeBuilder</div>
-  )
-}
+  const { resumeId } = useParams();
 
-export default ResumeBuilder
+  const [resumeData, setResumeData] = useState({ //resume details
+    _id: "",
+    title: "",
+    personal_info: {},
+    professinal_summary: {},
+    experience: [],
+    education: [],
+    projects: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3B82F6",
+    public: false,
+  });
+
+  const loadExistingResume = async () => { //db fetch to get the resume details if present
+    const resume = dummyResumeData.find((resume) => resume._id === resumeId);
+    if (resume) {
+      setResumeData(resume);
+      document.title = resume.title;
+    }
+  };
+
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0) //progress bar kosam
+  const [remove, setRemove] = useState(false)
+
+
+  const sections = [ //different sections of the resume
+    { id: "personal", name: "Personal Info", icon: User },
+    { id: "summary", name: "Summary", icon: FileText },
+    { id: "experience", name: "Experience", icon: Briefcase },
+    { id: "education", name: "Education", icon: GraduationCap },
+    { id: "projects", name: "Projects", icon: FolderIcon },
+    { id: "skills", name: "Skills", icon: Sparkles },
+  ]
+
+  const activeSection = sections[activeSectionIndex]
+
+  useEffect(() => { //page load avvagane edhi chey
+    loadExistingResume();
+  }, []);
+
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto px-4 py-6">
+        <Link to={"/app"}
+          className="inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all">
+          <ArrowLeftIcon className="size-4" /> Back to Dashboard
+        </Link>
+      </div>
+
+      <div className="max-w-7xl mx-auto px-4 pb-8">
+        <div className="grid lg:grid-cols-12 gap-8">
+          {/* left Panel */}
+          <div className="relative lg:col-span-5 rounded-lg overflow-hidden">
+            <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1 relative">
+              {/* progress bar using activeSectionIndex */}
+              <hr className="absolute top-0 left-0 w-full border-2 border-gray-200" />
+              <hr className="absolute top-0 left-0 h-1 bg-linear-to-r from-indigo-500 to-indigo-600 border-none transition-all duration-2000"
+                style={{
+                  width: `${(activeSectionIndex * 100) / (sections.length - 1)}%`,
+                }}
+              />
+
+              {/* section navigation */}
+              <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
+                {/* left side */}
+                <div>
+
+                </div>
+                {/* right side */}
+                <div className="flex items-center">
+                  <button
+                    //prevIndex is a inbuild method in useState for updating the state
+                    onClick={() => setActiveSectionIndex((prevIndex) => Math.max(0, prevIndex - 1))}
+                    disabled={activeSectionIndex === 0}
+                    className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${activeSectionIndex === 0 && "opacity-50"}`}
+                  >
+                    <ChevronLeft className="size-4" /> Previous
+                  </button>
+                  <button
+                    onClick={() => setActiveSectionIndex((prevIndex) => Math.min(sections.length - 1, prevIndex + 1))}
+                    className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${activeSectionIndex === sections.length - 1 && "opacity-50"}`} disabled={activeSectionIndex === sections.length - 1}>
+                    Next <ChevronRight className="size-4" />
+                  </button>
+                </div>
+
+              </div>
+
+              {/* form content */}
+              <div className="space-y-6">
+                {/* Render Personal Info Form only when active section is "personal" */}
+                {activeSection.id === "personal" && (
+                  <PersonalInfoForm
+                    data={resumeData.personal_info} // Pass only personal info data
+                    onChange={(data) => setResumeData(prev => ({ ...prev, personal_info: data }))} // Update only personal_info in main state
+                    removeBackground={remove}
+                    setRemoveBackground={setRemove}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+
+          {/* right panel -- Preview */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResumeBuilder;
